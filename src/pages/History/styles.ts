46f705893f72b7151b9951ac7929ee203ebbaf56
unknown <contato@alexbritto.com>
2022-11-17 01:22:27 -0300

    add history page

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,90 @@
+import styled, { css } from "styled-components";
+
+export const HistoryContainer = styled.main`
+  ${({ theme }) => css`
+    flex: 1;
+    padding: 3.5rem;
+
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+      font-size: 1.5rem;
+      color: ${theme.colors["gray-100"]};
+    }
+  `}
+`;
+
+export const HistoryList = styled.div`
+  ${({ theme }) => css`
+    flex: 1;
+    overflow: auto;
+    margin-top: 2rem;
+
+    table {
+      width: 100%;
+      border: collapse;
+      min-width: 600px;
+
+      th {
+        background-color: ${theme.colors["gray-600"]};
+        padding: 1rem;
+        text-align: left;
+        color: ${theme.colors["gray-100"]};
+        font-size: 0.875rem;
+        line-height: 1.6;
+
+        &:first-child {
+          border-top-left-radius: 8px;
+          padding-left: 1.5rem;
+        }
+
+        &:last-child {
+          border-top-right-radius: 8px;
+          padding-right: 1.5rem;
+        }
+      }
+
+      td {
+        background-color: ${theme.colors["gray-700"]};
+        border-top: 4px solid ${theme.colors["gray-800"]};
+        padding: 1rem;
+        font-size: 0.875rem;
+        line-height: 1.6;
+
+        &:first-child {
+          width: 50%;
+          padding-left: 1.5rem;
+        }
+
+        &:last-child {
+          padding-right: 1.5rem;
+        }
+      }
+    }
+  `}
+`;
+
+enum STATUS_COLORS {
+  green = "green-500",
+  yellow = "yellow-500",
+  red = "red-500",
+}
+
+//Para tipar uma prop com as chaves de um enum, é necessário utilizar o keyof do typeof deste ENUM
+
+export const Status = styled.span<{ statusColor: keyof typeof STATUS_COLORS }>`
+  ${({ theme, statusColor }) => css`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    &::before {
+      content: "";
+      width: 0.5rem;
+      height: 0.5rem;
+      border-radius: 50%;
+      background-color: ${theme.colors[STATUS_COLORS[statusColor]]};
+    }
+  `}
+`;
