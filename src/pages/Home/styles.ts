216e0aec5b062fcafa4098e27a9a6d64b076a2c5
unknown <contato@alexbritto.com>
2022-11-17 00:44:22 -0300

    style Home page

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -30,6 +30,40 @@ export const FormContainer = styled.div`
   `}
 `;
 
+const BaseInput = styled.input`
+  ${({ theme }) => css`
+    background-color: transparent;
+    height: 2.5rem;
+    border: 0;
+    border-bottom: 2px solid ${theme.colors["gray-500"]};
+    font-weight: bold;
+    font-size: 1.125rem;
+    color: ${theme.colors["gray-100"]};
+    padding: 0 0.5rem;
+
+    &:focus: {
+      box-shadow: none;
+      border-color: ${theme.colors["green-500"]};
+    }
+
+    &::placeholder {
+      color: ${theme.colors["gray-500"]};
+    }
+  `}
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`;
+
 export const CountdownContainer = styled.div`
   ${({ theme }) => css`
     font-family: "Roboto Mono", monospace;
@@ -57,5 +91,34 @@ export const Separator = styled.div`
     overflow: hidden;
     display: flex;
     justify-content: center;
+  `}
+`;
+
+export const StartCountDownButton = styled.button`
+  ${({ theme }) => css`
+    width: 100%;
+    border: 0;
+    padding: 1rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    gap: 0.5rem;
+    font-weight: bold;
+
+    cursor: pointer;
+
+    background-color: ${theme.colors["green-500"]};
+    color: ${theme.colors["gray-100"]};
+
+    &:disabled {
+      opacity: 0.7;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
+      background-color: ${theme.colors["green-700"]};
+    }
   `}
 `;
